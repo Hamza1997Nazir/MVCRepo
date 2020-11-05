866c7f5b00ce4ee808542dcd05e668bb8012426f
Hamza Nazir <hamza40nazir@gmail.com>
2020-11-05 16:23:09 +0500

    MVC project

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+
+        //GET : /Helloworld/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //GET /HelloWorld/Welcome/
+        public IActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewData["Message"] = "Mucho Gusto " + name;
+            ViewData["NumTimes"] = numTimes;
+            return View();
+            //return HtmlEncoder.Default.Encode($"Hello {name},ID {ID}");
+
+        }
+
+        
+    }
+}
